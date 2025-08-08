2832a4471852fdd0caed913c28d240de8d7b03bc
nimashomalii <106135440+nimashomalii@users.noreply.github.com>
2025-08-08 18:07:48 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,40 @@
 from dataset.main import data
 import torch 
 import os # os را برای چک کردن cuda اضافه کنید
+from models.simpleNN import model
+from train import Trainer
+import torch
+import plot 
+
+# ____________DATA SET __________#
+#the first step is to make datset ready for work 
 test_person = [2 , 15]
-overlap = 0.25
-time_len = 10
-# تعیین device به صورت صحیح
+overlap = 0.1
+time_len = 5
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 emotion= 'arousal'
 label_method = 'binary'
 batch_size = 126 
-
-# نمونه‌سازی کلاس
 my_dataset = data(test_person, overlap, time_len, device, emotion, label_method, batch_size)
+train_loader = my_dataset.train_data()
+test_loader = my_dataset.test_data()
 
-# حلقه زدن روی DataLoader که از متد train_data() برمی‌گردد.
-for x_train, y_train in my_dataset.train_data():
-    print(x_train.shape)
-    print(y_train.shape)
+#____Model______#
+Model = model([128, 64, 1])  # معماری دلخواه
+
+#____trainer_______#
+trainer = Trainer(
+    model=model,
+    train_loader=train_loader,
+    test_loader=test_loader,
+    device=device,
+    optimizer_cls=torch.optim.Adam,
+    lr=1e-3,
+    epochs=50,
+    checkpoint_path="eeg_checkpoint.pth",
+    log_path="eeg_log.json"
+)
+#____fit_model_____#
+history = trainer.fit()
+#____plot_result___#
+plot.plot_training_history(history)
