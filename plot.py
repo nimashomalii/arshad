1fdc0a781a9f362d3602b24e3e36de7f054a4ffd
nimashomalii <106135440+nimashomalii@users.noreply.github.com>
2025-08-09 12:20:59 +0330

    Update plot.py

--- plot.py
+++ plot.py
@@ -3,12 +3,10 @@ import matplotlib.pyplot as plt
 def plot_training_history(history):
     epochs = range(1, len(history['train_loss']) + 1)
 
-    plt.figure(figsize=(12, 5))
-
     # نمودار Loss
     plt.subplot(1, 2, 1)
     plt.plot(epochs, history['train_loss'], label='Train Loss')
-    plt.plot(epochs, history['test_loss'], label='Test Loss')
+    plt.plot(epochs, history['val_loss'], label='Test Loss')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.title('Loss over epochs')
@@ -17,7 +15,7 @@ def plot_training_history(history):
     # نمودار Accuracy
     plt.subplot(1, 2, 2)
     plt.plot(epochs, history['train_acc'], label='Train Accuracy')
-    plt.plot(epochs, history['test_acc'], label='Test Accuracy')
+    plt.plot(epochs, history['val_acc'], label='Test Accuracy')
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
     plt.title('Accuracy over epochs')
