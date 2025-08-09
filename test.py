ac32f16d77f15e64982b21584d3e82f06c7a1044
nimashomalii <106135440+nimashomalii@users.noreply.github.com>
2025-08-09 17:43:00 +0330

    Update test.py

--- test.py
+++ test.py
@@ -11,11 +11,11 @@ import matplotlib.pyplot as plt
 #the first step is to make datset ready for work 
 test_person = [2,9 , 15 , 20]
 overlap = 0.1
-time_len = 5
+time_len = 50
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-emotion= 'arousal'
+emotion= 'valence'
 label_method = 'binary'
-batch_size = 126 
+batch_size = 250
 data_type = torch.float32
 my_dataset = data(test_person, overlap, time_len, device, emotion, label_method, batch_size, data_type)
 train_loader = my_dataset.train_data()
@@ -43,6 +43,7 @@ print(history)
 plot.plot_training_history(history)
 plt.plot([1 , 2 , 3 , 4 ] , [ 0.24 , 0.87 , 0.62 , 0.78]) 
 plt.show()
+
 
 
 
