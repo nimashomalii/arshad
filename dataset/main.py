26f67278221951831b9641c05ef9f046d7d64741
nimashomalii <106135440+nimashomalii@users.noreply.github.com>
2025-08-09 10:54:20 +0330

    Update main.py

--- dataset/main.py
+++ dataset/main.py
@@ -10,7 +10,7 @@ import time
 
 import torch.nn as nn 
 # در فایل main.py
-def prepar_dataset(test_person, over_lap, time_len , device, emotion, label_method):
+def prepar_dataset(test_person, over_lap, time_len , device, emotion, label_method , data_type):
     with open('dataset/config.json', 'r') as f:
         config = json.load(f)
     file_id = config['file_id']
@@ -20,7 +20,7 @@ def prepar_dataset(test_person, over_lap, time_len , device, emotion, label_meth
     extract_data.extract_data_file(file_id)
 
     data_manage = dataset(test_person, over_lap, time_len, emotion, label_method)
-    data_manage.extract(file_path, torch.float64)
+    data_manage.extract(file_path, data_type)
     data_manage.normalize()
     x_train, x_test, y_train, y_test = data_manage.receive_data()
 
@@ -33,8 +33,8 @@ def prepar_dataset(test_person, over_lap, time_len , device, emotion, label_meth
 
     return x_train, x_test, y_train, y_test
 class data : 
-    def __init__(self , test_person, overlap, time_len, device, emotion, label_method, batch_size ) : 
-        self.x_train, self.x_test, self.y_train,  self.y_test = prepar_dataset(test_person, overlap, time_len, device, emotion, label_method)
+    def __init__(self , test_person, overlap, time_len, device, emotion, label_method, batch_size , data_type ) : 
+        self.x_train, self.x_test, self.y_train,  self.y_test = prepar_dataset(test_person, overlap, time_len, device, emotion, label_method , data_type)
         test_dataset = TensorDataset(self.x_test , self.y_test)
         self.test_loader = DataLoader(test_dataset ,batch_size , shuffle=True )
         train_dataset = TensorDataset(self.x_train , self.y_train )
