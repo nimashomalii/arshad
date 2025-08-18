ae55b807421d3b7d25d87b24fd0233ff7551c1ab
nimashomalii <106135440+nimashomalii@users.noreply.github.com>
2025-08-18 17:48:17 +0330

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 # ____________DATA SET __________#
 #the first step is to make datset ready for work 
-test_person = [2 ,12, 15 , 20]
+test_person = [2 ,8,12, 15 , 20]
 overlap = 0.4
 time_len = 5
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -40,6 +40,7 @@ trainer = Trainer(
 history = trainer.fit()
 #____plot_result___#
 plot.plot_training_history(history)
+
 
 
 
