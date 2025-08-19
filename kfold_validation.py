a648ee6e499355b7ef1c62b5d02bf62abf14387f
nimashomalii <106135440+nimashomalii@users.noreply.github.com>
2025-08-19 22:45:56 +0330

    Update kfold_validation.py

--- kfold_validation.py
+++ kfold_validation.py
@@ -30,7 +30,8 @@ def validate(model_name, emotion , k , num_people= 23) :
     pateints = k_fold_validation(k , num_people)
     len_patients = len(pateints)
     i = 0 
-    for test_person in pateints : 
+    for test_person in pateints :
+        print("a new procedure is takeing place . . . " ) 
         history = choose_model(model_name, emotion , test_person)
         if i ==0 : 
             train_loss = np.array(history['train_loss'])
@@ -50,4 +51,5 @@ def validate(model_name, emotion , k , num_people= 23) :
     return train_loss , val_loss , train_acc , val_acc
 
     
+
 
