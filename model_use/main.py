3147357d0a29d46a076e1a2f2203b74039cb87d1
nimashomalii <106135440+nimashomalii@users.noreply.github.com>
2025-08-19 10:40:23 +0330

    Update main.py

--- model_use/main.py
+++ model_use/main.py
@@ -1,5 +1,6 @@
 from model_use.simpleNN import create_model as simpleNN
 
-def choose_model(name , test_person) : 
+def choose_model(name , test_person , emotion) : 
     if name == 'simpleNN' : 
-        return simpleNN(test_person)
+        return simpleNN(test_person , emotion)
+
