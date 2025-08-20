57e52a232e7463e6928982f2af6cf41f73c76f90
nimashomalii <106135440+nimashomalii@users.noreply.github.com>
2025-08-20 13:06:07 +0330

    Update mainProject.py

--- mainProject.py
+++ mainProject.py
@@ -2,9 +2,10 @@ from kfold_validation import validate
 import sys
 from plot import plot_training_history
 emotion = sys.argv[2]
-k  = int(sys.argv[3])
+category  = sys.argv[3]
+k = int(sys.argv[4])
 model_name  = sys.argv[1]
-train_loss , val_loss , train_acc , val_acc = validate(model_name, emotion , k , 23)
+train_loss , val_loss , train_acc , val_acc = validate(model_name, emotion ,category, k , 23)
 history = {
     'train_loss' : train_loss , 
     'val_loss' : val_loss , 
