671d7e9507861c9f35eaade366acee63d95597d9
nimashomalii <106135440+nimashomalii@users.noreply.github.com>
2025-09-08 17:21:41 +0330

    Update main.py

--- dataset/main.py
+++ dataset/main.py
@@ -3,9 +3,9 @@ import json
 import sys
 from dataset.extractor import DataExtractor 
 from dataset.make_variable import dataset
-
+import random
 from  torch.utils.data import DataLoader , TensorDataset
-
+from dataset.dataset_subject_dependet import dataset as dataset_for_subjet_dependet
 import time
 
 import torch.nn as nn 
@@ -43,17 +43,33 @@ class data :
         return self.train_loader
     def  test_data(self ) : 
         return self.test_loader
+    
 
 
 
+def data_for_subject_dependet(overlap , time_len ,  emotion , label_method , data_type , device , person , k_fold) :
+    with open('dataset/config.json', 'r') as f:
+        config = json.load(f)
+    file_id = config['file_id']
+    file_path = config['data_paths']
+    extract_data = DataExtractor()
+    extract_data.extract_data_file(file_id)
+    sub_dep = dataset_for_subjet_dependet(overlap ,time_len , emotion , label_method)
+    n = 18//k_fold
+    r = 18- n*k_fold 
+    test_clips = [] 
+    all_clips = list(range(18))
+    random.shuffle(all_clips)
+    for i in range(k_fold) : 
+        test_clips.append(all_clips[i*n:(i+1)*n])
+    for i in range(r) : 
+        test_clips[i].append(all_clips[n*k_fold + i])
 
-
-
-
-
-
-
-
-
-
-
+    for test_clip in test_clips: 
+        x_train , y_train , x_test , y_test = sub_dep.extract(file_path, data_type , person , test_clip )
+        x_train = x_train.to(device)
+        x_test = x_test.to(device)
+        y_train = y_train.to(device)
+        y_test = y_test.to(device)
+        yield (x_train , x_test , y_train , y_test)
+    extract_data.clean_extracted_data()
