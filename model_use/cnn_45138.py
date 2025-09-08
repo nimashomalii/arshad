ede1153af19673ce01685c47442619c6ebbfeacb
nimashomalii <106135440+nimashomalii@users.noreply.github.com>
2025-09-08 19:06:07 +0330

    Update cnn_45138.py

--- model_use/cnn_45138.py
+++ model_use/cnn_45138.py
@@ -77,8 +77,8 @@ def subject_dependent_validation (emotion ,category, fold_idx , k=5) :
                 device=device,
                 label_method=category,
                 optimizer_cls=torch.optim.Adam,
-                lr=1e-3,
-                epochs=30,
+                lr=1e-4,
+                epochs=15,
                 checkpoint_path=f"eeg_checkpoint{fold_idx + person_num*5}.pth",
                 log_path=f"eeg_log{fold_idx + person_num*5}.json", 
             )
@@ -104,3 +104,4 @@ def subject_dependent_validation (emotion ,category, fold_idx , k=5) :
         accuracies_on_subjects['train'].append(np.max(np.array(train_acc)))
         accuracies_on_subjects['test'].append(np.max(np.array(val_acc)))
     return accuracies_on_subjects
+
