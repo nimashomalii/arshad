5b252ad5c20db1b67a2f9b0c12551d60dc46781f
nimashomalii <106135440+nimashomalii@users.noreply.github.com>
2025-10-28 20:27:36 +0330

    Update capsnet2020.py

--- models_structures/capsnet2020.py
+++ models_structures/capsnet2020.py
@@ -9,15 +9,17 @@ def x_to_cnn(input ) :
         data[:, :, i, j] = input[:, :, g]
     return data
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 class EmotionCaps(nn.Module):
     def __init__(self, num_emotions, out_dim, num_iterations=3):
-        super().__init__()
-        self.num_capsules = None
-        self.in_dim = None
+        super(EmotionCaps, self).__init__()
         self.num_emotions = num_emotions
         self.out_dim = out_dim
         self.num_iterations = num_iterations
-        self.W = None  # lazy init
+        self.W = None
 
     def squash(self, s, dim=-1):
         norm = torch.norm(s, dim=dim, keepdim=True)
@@ -26,46 +28,34 @@ class EmotionCaps(nn.Module):
 
     def forward(self, u):
         B, N, in_dim = u.size()
-
         if self.W is None:
             self.num_capsules = N
             self.in_dim = in_dim
-            self.W = nn.Parameter(
-                torch.randn(1, N, self.num_emotions, self.out_dim, in_dim, device=u.device)
-            )
-
+            self.W = nn.Parameter(torch.randn(1, N, self.num_emotions, self.out_dim, in_dim, device=u.device))
         u_exp = u.unsqueeze(2).unsqueeze(-1)
         u_hat = torch.matmul(self.W, u_exp).squeeze(-1)
         b = torch.zeros(B, self.num_capsules, self.num_emotions, device=u.device)
-
         for r in range(self.num_iterations):
             c = F.softmax(b, dim=2).unsqueeze(-1)
             s = (c * u_hat).sum(dim=1)
-            v = self.squash(s)  # <--- اینجا باید squash باشه
-
+            v = self.squash(s)
             if r < self.num_iterations - 1:
                 v_exp = v.unsqueeze(1).unsqueeze(-1)
                 uv = torch.matmul(u_hat.unsqueeze(-2), v_exp).squeeze(-1).squeeze(-1)
                 b = b + uv
-
         return v
 
-
 class model(nn.Module):
-    def __init__(self, num_filter, time_len, caps_len, num_emotions, out_dim):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=num_filter, kernel_size=6, stride=(2,1), padding=0) #(B , 1 , 62 , 9)
-        self.conv2 = nn.Conv2d(in_channels=num_filter, out_channels=num_filter, kernel_size=5, stride=1 , padding=2)
-        # bottleneck 1x1
-        self.conv3 = nn.Conv2d(in_channels=2 * num_filter, out_channels=num_filter, kernel_size=1)
-        self.padd_layer = nn.ZeroPad2d((2,3,2,3))
+    def __init__(self, num_filter=256, caps_len=8, num_emotions=2, out_dim=16):
+        super(EEGEmotionCapsNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, num_filter, kernel_size=6, stride=(2, 1), padding=0)
+        self.conv2 = nn.Conv2d(num_filter, num_filter, kernel_size=5, stride=1, padding=2)
+        self.conv3 = nn.Conv2d(2 * num_filter, num_filter, kernel_size=1)
         self.relu = nn.ReLU()
         self.caps_len = caps_len
-
         self.emotion_caps = EmotionCaps(num_emotions=num_emotions, out_dim=out_dim)
 
     def forward(self, x):
-        # x: (B, 1, time_len , num_channel)
         x = x.unsqueeze(1)
         x = self.conv1(x)
         x = self.relu(x)
@@ -75,9 +65,8 @@ class model(nn.Module):
         x = self.relu(x)
         x = self.conv3(x)
         x = x.view(x.size(0), -1, self.caps_len)
-        # emotion capsules
-        v = self.emotion_caps(x)  # (B, M, out_dim)
-        v_abs = torch.norm(v , dim=-1)
+        v = self.emotion_caps(x)
+        v_abs = torch.norm(v, dim=-1)
         return v_abs
 
 
